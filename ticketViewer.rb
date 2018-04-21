22c5b65b33b2430fb69a80d6cb6151d2e7db0e23
raphsutti <neoster9@gmail.com>
2018-04-21 21:05:22 +1000

    GET request app

--- ticketViewer.rb
+++ ticketViewer.rb
@@ -1,1 +1,20 @@
-puts "hey!"
+# require './menu.rb'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'openssl'
+
+uri = URI.parse("https://firstorder.zendesk.com/api/v2/tickets.json")
+
+Net::HTTP.start(uri.host, uri.port,
+  :use_ssl => uri.scheme == 'https', 
+  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+  request = Net::HTTP::Get.new uri.request_uri
+  request.basic_auth 'r.suttiyotin@gmail.com', 'test1234'
+
+  response = http.request request
+
+  puts response.code
+  # puts response.body
+end
