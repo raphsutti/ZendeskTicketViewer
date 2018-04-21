05d86c4f315c1888d774842800ef04967ba6ac1c
raphsutti <neoster9@gmail.com>
2018-04-21 20:41:36 +1000

    menu display file

---
+++ menu.rb
@@ -0,0 +1,12 @@
+puts ""
+puts "--- First Order Ticket Viewer ---"
+puts "Type 'help' to view options or 'quit' to exit"
+
+puts ""
+puts "Options:"
+puts "'view' - display all tickets"
+puts "'view {id}' - display ticket by id"
+
+input = gets.chomp
+
+puts "hello " + input
