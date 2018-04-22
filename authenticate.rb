92156ea09cc45c4c5e9eb832304402603b4033c5
raphsutti <neoster9@gmail.com>
2018-04-22 14:34:39 +1000

    initial commit

---
+++ authenticate.rb
@@ -0,0 +1,29 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'openssl'
+
+def authenticate(username, password)
+
+  uri = URI.parse("https://firstorder.zendesk.com/api/v2/tickets.json")
+
+  Net::HTTP.start(uri.host, uri.port,
+    :use_ssl => uri.scheme == 'https', 
+    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+    request = Net::HTTP::Get.new uri.request_uri
+    request.basic_auth username, password
+
+    response = http.request request
+    
+    if response.code.to_i != 200
+      puts "Failed to authenticate, error code: " + response.code
+      return false
+    end
+    # puts response.body
+    # parsed_json = JSON.parse(response.body)
+    # puts parsed_json['tickets'][0]['description']
+    # 
+
+  end
+end
