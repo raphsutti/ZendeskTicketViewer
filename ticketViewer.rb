31e580641a54f9d1bc50aad52335f6652fc0ff0e
raphsutti <neoster9@gmail.com>
2018-04-22 17:39:01 +1000

    display all tickets from multiple pages

--- ticketViewer.rb
+++ ticketViewer.rb
@@ -6,31 +6,37 @@ require 'openssl'
 require './pageCounter.rb'
 
 pageCount = pageCounter()
-puts "pageCount"
 # for count > 100, another page request required
+currentPage = 1
 
-uri = URI.parse("https://firstorder.zendesk.com/api/v2/tickets.json?page=#{page}")
+# loop through all the pages
+while currentPage <= pageCount do
 
-Net::HTTP.start(uri.host, uri.port,
-  :use_ssl => uri.scheme == 'https', 
-  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+  uri = URI.parse("https://firstorder.zendesk.com/api/v2/tickets.json?page=#{currentPage}")
 
-  request = Net::HTTP::Get.new uri.request_uri
-  request.basic_auth 'r.suttiyotin@gmail.com', 'test1234'
+  Net::HTTP.start(uri.host, uri.port,
+    :use_ssl => uri.scheme == 'https', 
+    :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
 
-  response = http.request request
-  
-  parsed_json = JSON.parse(response.body)
-  ticketArray = parsed_json['tickets']
-  count = parsed_json['count']
-  
-  ticketArray.each do |i|
-    puts "----- id:#{i['id']} -----"
-    puts "#{i['description']}"
-    puts "----- end of ticket -----"
-    puts ""  
+    request = Net::HTTP::Get.new uri.request_uri
+    request.basic_auth 'r.suttiyotin@gmail.com', 'test1234'
+
+    response = http.request request
+    
+    parsed_json = JSON.parse(response.body)
+    ticketArray = parsed_json['tickets']
+    count = parsed_json['count']
+    
+    ticketArray.each do |i|
+      puts "----- id:#{i['id']} -----"
+      puts "#{i['description']}"
+      puts "----- end of ticket -----"
+      puts ""  
+    end
+
+    puts "Total ticket count: #{count}"
+
   end
 
-  puts "Total ticket count: #{count}"
-
+  currentPage +=1
 end
